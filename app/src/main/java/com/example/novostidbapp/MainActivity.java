8c96619d099fb3f1d242b135490a05bbb42ebc55
isip_a.d.shubin@mpt.ru <mrabdulrhman9@gmail.com>
2022-04-24 00:40:55 +0300

    NovostiDBApp Version 1.2 -Added bio

--- app/src/main/java/com/example/novostidbapp/MainActivity.java
+++ app/src/main/java/com/example/novostidbapp/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.novostidbapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.concurrent.Executor;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText login,password;
-    Button avtoriz,registr;
+    Button avtoriz,registr,btnScan;
     DataBaseHelperUsers databaseHelper;
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
         registr = findViewById(R.id.button2);
         login = findViewById(R.id.login);
         password = findViewById(R.id.password);
-
+        btnScan = findViewById(R.id.btnScan);
+        executor = ContextCompat.getMainExecutor(this);
         avtoriz.setOnClickListener(view ->{
                     Cursor res = databaseHelper.getdataUsers();
                     if(res.getCount() == 0)
@@ -67,5 +77,36 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(back);
                 }
         );
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Log.e("ErrorAUTH", errString.toString());
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(MainActivity.this, "Успешно вход в админа!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,Admin.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Log.e("FailedAUTH", "Fail!");
+            }
+        });
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Авторизация")
+                .setSubtitle("Прислонить палец")
+                .setNegativeButtonText("Отмена")
+                .build();
+
+
+        btnScan.setOnClickListener(view -> {
+            biometricPrompt.authenticate(promptInfo);
+        });
     }
 }
